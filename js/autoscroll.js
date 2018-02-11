79809c42187aeafbf7b43a8ced45d51070e8f5e9
mram <mram@umd.edu>
2018-02-11 18:42:47 -0500

    Added websocket setup

--- js/autoscroll.js
+++ js/autoscroll.js
@@ -2,8 +2,8 @@ $(document).ready(
     function () {
         $("#autoscroll").on("click", function () {
             if($(this).is(":checked")) {
-                const textarea = $("#comms");
-                const bottom = textarea.prop('scrollHeight') - textarea.height();
+                let textarea = $("#comms");
+                let bottom = textarea.prop('scrollHeight') - textarea.height();
 
                 textarea.animate({
                     scrollTop: bottom
--- js/communication.js
+++ js/communication.js
@@ -1,7 +1,12 @@
 $(document).ready(
 
     function () {
-        // console.log("Test");
+        let connection = new WebSocket('ws://echo.websocket.org/');
+
+        connection.onopen = () => { connection.send('Ping'); };
+        connection.onerror = error => { console.log('WebSocket Error: ' + error); };
+        connection.onmessage = e => { console.log('Server: ' + e.data); };
+
     }
 
 );
