6c3ab8a037d26027c04441d8dffd385a17fa5498
mram <mram@umd.edu>
2018-02-12 01:13:43 -0500

    Commented out websocket stuff

--- js/communication.js
+++ js/communication.js
@@ -1,12 +1,12 @@
-$(document).ready(
-
-    function () {
-        let connection = new WebSocket('ws://echo.websocket.org/');
-
-        connection.onopen = () => { connection.send('Ping'); };
-        connection.onerror = error => { console.log('WebSocket Error: ' + error); };
-        connection.onmessage = e => { console.log('Server: ' + e.data); };
-
-    }
-
-);
+// $(document).ready(
+//
+//     function () {
+//         let connection = new WebSocket('wss://echo.websocket.org/');
+//
+//         connection.onopen = () => { connection.send('Ping'); };
+//         connection.onerror = error => { console.log('WebSocket Error: ' + error); };
+//         connection.onmessage = e => { console.log('Server: ' + e.data); };
+//
+//     }
+//
+// );
