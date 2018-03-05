15f04e2923381d4295a327c8ff3c579280b392b6
mram <mram@umd.edu>
2018-03-05 15:35:30 -0500

    Updated server

--- server.py
+++ server.py
@@ -2,15 +2,5 @@
 
 import os
 
-import SimpleHTTPServer
-import requests
-
 PORT = 8000
-os.system("freeport" + PORT)
-# URL = 'localhost:{port}'.format(port=PORT)
-
-Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
-server = SocketServer.TCPServer(("", PORT), Handler)
-print "Serving client on port", PORT
-
-server.serve_forever()
+os.system("python -m SimpleHTTPServer " + str(PORT))
