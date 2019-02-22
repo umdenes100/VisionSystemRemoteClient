e99db271926276ac21484d8f2a1a7001fe77f120
mram <mram@umd.edu>
2019-02-22 18:08:09 -0500

    Completed port change stuff

--- js/communication_alt.js
+++ js/communication_alt.js
@@ -1,29 +1,99 @@
-const WEBSOCKET_ADDRESS = 'ws://192.168.1.2:9000/';
-const connection = new WebSocket(WEBSOCKET_ADDRESS);
+const WEBSOCKET_ADDRESS = 'ws://192.168.1.2:9000/'
+const connection = new WebSocket(WEBSOCKET_ADDRESS)
+
+let selectedPort = ''
+
+function changePort() {
+    let newPort = $('#port').val()
+    let portProtocol
+
+    if (newPort === '') {
+        portProtocol = {
+            PORT: selectedPort,
+            TYPE: 'SOFT_CLOSE'
+        }
+    } else {
+        if (selectedPort === '') {
+            portProtocol = {
+                PORT: newPort,
+                TYPE: 'OPEN'
+            }
+        } else {
+            portProtocol = {
+                PORT: selectedPort,
+                TYPE: 'SWITCH',
+                NEW_PORT: newPort
+            }
+        }
+    }
+
+    selectedPort = newPort
+
+    connection.send(JSON.stringify(portProtocol))
+}
+
+$(window).on('unload', e => {
+    let portProtocol = {
+        PORT: selectedPort,
+        TYPE: 'HARD_CLOSE'
+    }
+    connection.send(JSON.stringify(portProtocol))
+})
 
 $(document).ready(() => {
+
+    $port = $("#port")
 
     connection.onopen = () => {
         console.log('OPEN')
     };
 
     connection.onerror = error => {
-        console.log('WebSocket Error.');
+        console.log('WebSocket Error.')
     };
 
     connection.onmessage = message => {
-        console.log('OPEN')
-        let data = JSON.parse(message.data);
-        console.log(data);
-        // connection.send("It's Lit")
+        message = JSON.parse(message.data)
+        let type = message['TYPE']
+        let content = message['CONTENT']
+        console.log(message)
+
+        switch(type) {
+            case 'PORT_LIST':
+                $port.empty()
+                $port.append($("<option>", {
+                    value : '',
+                    text : "Select Team Here..."
+                }))
+
+                Object.keys(content).map((port, index) => {
+                    if (port !== content[port]['NAME']) {
+                        $port.append($("<option>", {
+                            value : port,
+                            text : content[port]['NAME']
+                        }))
+                    }
+                })
+
+                if (Object.keys(content).includes(selectedPort))  {
+                    $port.val(selectedPort)
+                }
+                break
+
+            default:
+                break
+        }
+        
     };
 
     connection.onclose = () => {
-        console.log('CLOSE');
-        connection.send('Closed.');
+        console.log('CLOSE')
+        connection.send('Closed.')
         setTimeout(() => {
-            console.log("Retrying...");
-            start();
-        }, 5000);
-    };
+            console.log("Retrying...")
+            start()
+        }, 5000)
+    }
+
+    $port.on('change', changePort)
 })
