5a449442f7cfda7163516c793471b2c6be380db9
mram <mram@umd.edu>
2019-05-02 14:49:33 -0400

    Cleanup

--- js/simulator/communication.js
+++ js/simulator/communication.js
@@ -20,35 +20,6 @@ $(document).ready(() => {
 
     request_randomization()
 
-
-    // connection = new WebSocket("ws://18.191.246.34:8888/")
-    //
-    // connection.onopen = () => {
-    //     console.log('OPEN')
-    //     connection.send(JSON.stringify({
-    //         type: 'randomization'
-    //     }))
-    // }
-    //
-    // connection.onerror = error => {
-    //     console.log(`WebSocket Error.: ${error}`)
-    // }
-    //
-    // connection.onmessage = message => {
-    //
-    //     message = JSON.parse(message.data)
-    //     if (message.type === 'randomization') {
-    //
-
-    //     } else {
-    //         console.log('Unimplemented')
-    //     }
-    // }
-    //
-    // connection.onclose = () => {
-    //     console.log('Failed')
-    // }
-    //
     $('#randomize').on('click', request_randomization)
     //
     // $('#simulate').on('click', () => {
