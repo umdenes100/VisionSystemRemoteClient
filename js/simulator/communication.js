97a0b0796953a4183262bafa62ce661945dc8452
mram <mram@umd.edu>
2019-05-02 14:48:57 -0400

    websockets => http

--- js/simulator/communication.js
+++ js/simulator/communication.js
@@ -1,51 +1,59 @@
 let randomization = undefined
 let connection = undefined
 
+const SERVER_URL = 'http://18.191.246.34:8888'
+
+function request_randomization() {
+    let request = { type: 'randomization' }
+    $.get(SERVER_URL, request, data => {
+        let canvasses = [pcanvas, canvas]
+
+        canvasses.map(canv => {
+            canv.obstacles = data.obstacles.map(obstacle => new Obstacle(obstacle.x, obstacle.y))
+            canv.destination = new Destination(data.destination.x, data.destination.y)
+            canv.draw()
+        })
+    })
+}
+
 $(document).ready(() => {
 
-    connection = new WebSocket("ws://18.191.246.34:8888/")
+    request_randomization()
 
-    connection.onopen = () => {
-        console.log('OPEN')
-        connection.send(JSON.stringify({
-            type: 'randomization'
-        }))
-    }
 
-    connection.onerror = error => {
-        console.log(`WebSocket Error.: ${error}`)
-    }
+    // connection = new WebSocket("ws://18.191.246.34:8888/")
+    //
+    // connection.onopen = () => {
+    //     console.log('OPEN')
+    //     connection.send(JSON.stringify({
+    //         type: 'randomization'
+    //     }))
+    // }
+    //
+    // connection.onerror = error => {
+    //     console.log(`WebSocket Error.: ${error}`)
+    // }
+    //
+    // connection.onmessage = message => {
+    //
+    //     message = JSON.parse(message.data)
+    //     if (message.type === 'randomization') {
+    //
 
-    connection.onmessage = message => {
-
-        message = JSON.parse(message.data)
-        if (message.type === 'randomization') {
-
-            let canvasses = [pcanvas, canvas]
-
-            canvasses.map(canv => {
-                canv.obstacles = message.obstacles.map(obstacle => new Obstacle(obstacle.x, obstacle.y))
-                canv.destination = new Destination(message.destination.x, message.destination.y)
-                canv.draw()
-            })
-        } else {
-            console.log('Unimplemented')
-        }
-    }
-
-    connection.onclose = () => {
-        console.log('Failed')
-    }
-
-    $('#randomize').on('click', () => {
-        connection.send(JSON.stringify({
-            type: 'randomization'
-        }))
-    })
-
-    $('#simulate').on('click', () => {
-        $("html, body").animate({ scrollTop: $(document).height() }, "slow");
-    })
+    //     } else {
+    //         console.log('Unimplemented')
+    //     }
+    // }
+    //
+    // connection.onclose = () => {
+    //     console.log('Failed')
+    // }
+    //
+    $('#randomize').on('click', request_randomization)
+    //
+    // $('#simulate').on('click', () => {
+    //     $("html, body").animate({ scrollTop: $(document).height() }, "slow");
+    // })
 
 })
 
