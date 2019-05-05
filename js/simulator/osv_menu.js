006cf044b44529ad44d74ec7f2f315d9d6b9aac7
mram <mram@umd.edu>
2019-05-05 19:44:49 -0400

    Style changes

--- js/simulator/osv_menu.js
+++ js/simulator/osv_menu.js
@@ -1,16 +1,67 @@
 let mcanvas = undefined
 
-class Sensor {
-    constructor() {
+const sensors = ([...Array(12).keys()]).map(no => {
+    let side = undefined
+    if ([0, 1, 2].includes(no)) {
+        side = 'right'
+    } else if ([3, 4, 5].includes(no)) {
+        side = 'down'
+    } else if ([6, 7, 8].includes(no)) {
+        side = 'left'
+    } else if ([9, 10, 11].includes(no)) {
+        side = 'up'
+    }
+    let orientation = undefined
+    if (side === 'right' || side === 'left') {
+        orientation = 'vertical'
+    } else if (side === 'up' || side === 'down') {
+        orientation = 'horizontal'
+    }
 
+    return {
+        no,
+        orientation,
+        side,
+    }
+})
+
+
+class Sensor {
+    constructor(number) {
+        self.number = number
+        self.vertical = false
+        self.inverted = false
     }
 
     resize(canvasWidth, canvasHeight) {
+        this.plate_width = (canvasWidth * 0.8) / 3
+        this.plate_height = canvasHeight * 0.01
 
+        this.cylinder_width = this.plate_width / 5
+        this.cylinder_height = this.plate_width / 5
+
+
+        this.x = 0
+        this.y = canvasHeight * 0.05
     }
 
     draw(context) {
 
+        if (self.vertical) {
+            context.fillStyle = '#0a2869'
+            context.fillRect(0, this.y, this.plate_height, this.plate_width)
+
+            context.fillStyle = '#6d6d6d'
+            context.fillRect(this.x + this.plate_height, this.y + this.plate_width / 5, this.cylinder_height, this.cylinder_width)
+            context.fillRect(this.x + this.plate_height, this.y + this.plate_width * 3 / 5, this.cylinder_height, this.cylinder_width)
+        } else {
+            context.fillStyle = '#0a2869'
+            context.fillRect(0, this.y, this.plate_width, this.plate_height)
+
+            context.fillStyle = '#6d6d6d'
+            context.fillRect(this.x + this.plate_width / 5, this.y + this.plate_height, this.cylinder_width, this.cylinder_height)
+            context.fillRect(this.x + this.plate_width * 3 / 5, this.y + this.plate_height, this.cylinder_width, this.cylinder_height)
+        }
     }
 }
 
@@ -24,8 +75,8 @@ class MenuOSV {
     }
 
     resize(canvasWidth, canvasHeight) {
-        this.width = canvasWidth * this.actual_width / 500
-        this.height = canvasHeight * this.actual_height / 500
+        this.width = canvasWidth * this.actual_width / 800
+        this.height = canvasHeight * this.actual_height / 800
 
         this.x = (canvasWidth - this.width) / 2
         this.y = (canvasHeight - this.height) / 2
@@ -35,11 +86,11 @@ class MenuOSV {
 
         context.fillStyle = this.tread_color
         context.fillRect(this.x - this.width * 0.1,
-                         this.y - (this.height * 0.15),
+                         this.y - (this.height * 0.2),
                          this.width * 1.2,
                         this.height * 0.15)
         context.fillRect(this.x - this.width * 0.1,
-                        this.y + this.height,
+                        this.y + this.height * 1.05,
                         this.width * 1.2,
                         this.height * 0.15)
 
@@ -65,6 +116,7 @@ class MenuCanvas {
         this.context = this.canvas.getContext('2d')
 
         this.osv = new MenuOSV(250, 200)
+        this.sensors = new Sensor(0)
     }
 
     width() {
@@ -79,8 +131,12 @@ class MenuCanvas {
         this.canvas.width = this.width()
         this.canvas.height = this.height()
 
-        this.osv.resize(this.width(), this.height())
-        this.osv.draw(this.context)
+        this.elements().map(element => {
+            if (element !== undefined) {
+                element.resize(this.width(), this.height())
+                element.draw(this.context)
+            }
+        })
     }
 
     resize(elements_to_resize) {
@@ -95,6 +151,10 @@ class MenuCanvas {
             document.getElementById(elementId)
                     .setAttribute('style',`height:${this.height()}px`)
         })
+    }
+
+    elements() {
+        return [this.osv, this.sensors].flat()
     }
 
 }
