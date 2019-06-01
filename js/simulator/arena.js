2eac1e0e59cd658853c0e5ca08b549a47f449d3f
Karam Singh <karamsingh175@gmail.com>
2019-05-31 20:08:39 -0400

    rotating backwards

--- js/simulator/arena.js
+++ js/simulator/arena.js
@@ -123,7 +123,7 @@ class OSV {
     draw(context) {
         console.log(this.actualTheta)
         context.translate(this.x + this.width / 2, this.y + this.height / 2)
-        context.rotate(this.actualTheta)
+        context.rotate(-this.actualTheta)
         context.fillStyle = this.color
         context.fillRect(-this.width / 2, -this.height / 2, this.width, this.height)
         context.translate(0, 0)
