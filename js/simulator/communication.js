eee15cad36174c7fc0580c853d016f1f7ee95fa5
Karam Singh <karamsingh175@gmail.com>
2019-06-02 02:24:27 -0400

    fixing constants

--- js/simulator/communication.js
+++ js/simulator/communication.js
@@ -12,7 +12,7 @@ function requestRandomization() {
 
         randomization = data
         canvasses.map(canv => {
-            canv.osv = new OSV(data.osv.x, data.osv.y, data.osv.theta, mcanvas.osv.actualWidth / 100, mcanvas.osv.actualHeight / 100)
+            canv.osv = new OSV(data.osv.x, data.osv.y, data.osv.theta, mcanvas.osv.actualWidth / 1000, mcanvas.osv.actualHeight / 1000)
             canv.obstacles = data.obstacles.map(obstacle => new Obstacle(obstacle.x, obstacle.y))
             canv.destination = new Destination(data.destination.x, data.destination.y)
             canv.draw()
