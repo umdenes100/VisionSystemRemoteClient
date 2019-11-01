a2a60cc6348227e952a730e7adee1bdcd53ab411
Jet Lee <gamestar109@live.com>
2019-11-01 15:01:09 -0400

    Delete bugreport.js

--- js/bugreport.js
+++
@@ -1,8 +0,0 @@
-function openbugreport() {
-    console.log("You clicked bug report fam");
-}
-
-$(document).ready(() => {
-    console.log("Bug report script is starting.");
-    $('#bugReport.nav-link').on('click', openbugreport);
-})
