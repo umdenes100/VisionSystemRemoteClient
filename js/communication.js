ad9ddc42863264f0e7d183eb667befa230efc631
Gary <sneed.garrett@gmail.com>
2022-04-25 14:05:56 -0400

    trying diff wss

--- js/communication.js
+++ js/communication.js
@@ -1,4 +1,10 @@
 const WEBSOCKET_ADDRESS = 'wss://192.168.1.2:9000/'
+const WEBSOCKET_ADDRESS = new WebSocket('wss://192.168.1.2:9000/', {
+  protocolVersion: 8,
+  origin: 'https://192.168.1.2:9000',
+  rejectUnauthorized: false
+});
+
 let connection = undefined
 
 let selectedPort = ''
